1a46abd54ffb1afe19b76c976266fe96792a3c1c
CodeSpikey <codespikex@gmail.com>
2024-03-19 21:34:19 +0500

    Refactor auth middleware to remove unnecessary code

--- src/components/widgets/middleware/auth.ts
+++ src/components/widgets/middleware/auth.ts
@@ -5,14 +5,8 @@ type Options = {
 	onFail?: () => void
 }
 
-const wait = (ms: number) =>
-	new Promise(r => setTimeout(() => r(undefined), ms))
-
 export async function auth(opts: Options, next: NextFn) {
 	await opts.onAuth?.()
-	console.log("authenticating...")
-	await wait(1000)
-	console.log("authenticated")
 
 	next()
 }
